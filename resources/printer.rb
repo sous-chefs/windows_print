5d2daee4d0329bbe6d0ab2bc73bab989e052a52b
unknown <dgroh@ITS-DGROH.ARCH.TAMU.EDU>
2013-12-05 16:14:11 -0600

    Allow creation of printer if port and driver exist

--- resources/printer.rb
+++ resources/printer.rb
@@ -28,16 +28,18 @@ actions :create, :delete
 
 default_action :create
 
-attribute :printer_name, :name_attribute => true, :kind_of => String, :required => true 
+attribute :printer_name, :name_attribute => true, :kind_of => String, :required => true
+attribute :driver_name, :kind_of => String, :required => true
+attribute :port_name, :kind_of => String, :required => true
+attribute :share_name, :kind_of => String
+attribute :location, :kind_of => String
 attribute :comment, :kind_of => String
-attribute :shared, :kind_of => [ TrueClass, FalseClass ],
-            :default => false
-attribute :driver_name, :kind_of => String
-attribute :port_name, :kind_of => String
-attribute :ipv4_address, :kind_of => String, :regex => Resolv::IPv4::Regex
-attribute :inf_path, :kind_of => String
-attribute :inf_file, :kind_of => String
+
+# Required for driver creation
 attribute :version, :kind_of => String, :default => "Type 3 - User Mode"
 attribute :environment, :kind_of => String, :default => "x64"
-attribute :location, :kind_of => String
-attribute :share_name, :kind_of => String
+attribute :inf_path, :kind_of => String
+attribute :inf_file, :kind_of => String
+
+# Required for port creation
+attribute :ipv4_address, :kind_of => String, :regex => Resolv::IPv4::Regex
