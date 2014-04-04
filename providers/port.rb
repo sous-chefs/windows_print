291863487ccda95768587ee974f23fda04671edc
Derek Groh <dgroh@arch.tamu.edu>
2014-04-04 13:03:27 -0500

    foodcritic

--- providers/port.rb
+++ providers/port.rb
@@ -26,7 +26,7 @@ action :create do
       Chef::Log.info{"#{new_resource.port_name} already created - nothing to do."}
       new_resource.updated_by_last_action(false)
     else
-      powershell_script "#{new_resource.port_name}" do
+      powershell_script "new_resource.port_name" do
         code "Add-PrinterPort -Name \"#{new_resource.port_name}\" -PrinterHostAddress \"#{new_resource.ipv4_address}\""
     end
     
@@ -38,7 +38,7 @@ action :create do
 
 action :delete do
   if port_exists?
-    powershell_script "#{new_resource.port_name}" do
+    powershell_script "new_resource.port_name" do
       code "Remove-PrinterPort -Name \"#{new_resource.port_name}\""
     end
     new_resource.updated_by_last_action(true)
